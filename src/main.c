c44e5467633e243e2b67f53abc18ee204e4958ed
ruhomor <ruhomor@icloud.com>
2020-09-05 02:54:16 +0300

    wtf is going on

--- src/main.c
+++ src/main.c
@@ -4,10 +4,12 @@
 int		main(void)
 {
 
-	printf("original: emem %#X meme\n", 0);
-	ft_printf("myprintf: emem %#X meme\n", 0);
-	printf("original: emem %#x meme\n", 0);
-	ft_printf("myprintf: emem %#x meme\n", 0);
+ 	printf("original: emem %#X meme\n", 7);
+	ft_printf("myprintf: emem %#X meme\n", 7);
+	printf("original: emem %#.x meme\n", 0);
+	ft_printf("myprintf: emem %#.x meme\n", 0);
+	printf("original: emem %#.0x meme\n", 2);
+	ft_printf("myprintf: emem %#.0x meme\n", 2);
 	printf("original: emem %#o meme\n", 0);
 	ft_printf("myprintf: emem %#o meme\n\n\n", 0);
 	printf("original: emem %#.o meme\n", 0);
