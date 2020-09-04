b4c9321f417ee4f2c780b61949f2c9228d3aa812
ruhomor <ruhomor@icloud.com>
2020-09-04 21:24:57 +0300

    c fix #2

--- src/main.c
+++ src/main.c
@@ -3,7 +3,11 @@
 
 int		main(void)
 {
-	printf("original: emem %5c meme\n", '\0');
-	ft_printf("myprintf: emem %5c meme\n", '\0');
+	printf("original: emem |%5c| meme\n", '\0');
+	ft_printf("myprintf: emem |%5c| meme\n", '\0');
+	printf("original: emem %-5c meme\n", '\0');
+	ft_printf("myprintf: emem %-5c meme\n", '\0');
+	printf("original: emem %1c meme\n", '\0');
+	ft_printf("myprintf: emem %1c meme\n", '\0');
 	return (0);
 }
